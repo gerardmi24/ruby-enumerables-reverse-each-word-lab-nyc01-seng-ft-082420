a35b028ad4b88a352b6655157169795b855f266e
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-08-19 01:54:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,11 @@
 require "pry"
 
 string = "Hello there, and how are you?"
-new_array = []
+
 def reverse_each_word(string)
 
     array = string.split
 
     array.collect {|element| element.reverse}
-    new_array << array
 
 end
