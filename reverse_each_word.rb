10f7dae316a043a8fdc70bb66c94ae12bdec8baf
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-08-19 01:50:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,9 @@ require "pry"
 
 string = "Hello there, and how are you?"
 def reverse_each_word(string)
-  binding.pry
+
     array = string.split
-    binding.pry
+
     array.collect {|element| element.reverse}
 binding.pry
 end
