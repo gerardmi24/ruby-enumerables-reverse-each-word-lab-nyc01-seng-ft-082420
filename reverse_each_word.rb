9673c83136f546523820709d7bd59d081f932079
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-08-19 02:22:57 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+#require "pry"
+
+string = "Hello there, and how are you?"
+def reverse_each_word(string)
+    array = string.split
+#binding.pry
+    array.collect {|element| element.reverse}.join(" ")
+#binding.pry
+end
